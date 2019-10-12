d97bfc4ffea48d65f848c9e17c3d33efebf0678e
AARON M <asmarcks@gmail.com>
2019-10-12 17:36:00 -0500

    set up express and listening function, defined routes

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 //dependencies 
 var express = require('express');
 var bodyParser = require('body-parser');
+var http = require('http');
 
-
+//set up express and use body-parsing middleware
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//defining external routes
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes");
+
+//listen function
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});

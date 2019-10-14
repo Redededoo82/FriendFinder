8cb1e29a6dbec50f4e7eaa1d39574e4dad9e8b0a
AARON M <asmarcks@gmail.com>
2019-10-13 20:33:59 -0500

    survey page almost complete, html routes set up

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
+
 //dependencies 
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
-var path = require('path');
-
 //set up express and use body-parsing middleware
 var app = express();
-var PORT = process.env.PORT || 8080;
+var port = process.env.port || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
-app.use(bodyParser.json({type: "application/vnd.api+json"}));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+ require("./app/routes/apiRoutes")(app);
+ require("./app/routes/htmlRoutes")(app);
 
-
-//defining external routes
- require("./app/routing/apiRoutes.js");
- require("./app/routing/htmlRoutes.js");
-
-//listen function
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+ app.listen(port, function() {
+    console.log("App listening on http://localhost:" + port);
+  });
